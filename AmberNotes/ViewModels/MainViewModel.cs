a40c3802a120c2f159285b311af10d1ee31bc3f3
r_kozak <roman.kozak2085@gmail.com>
2026-05-03 12:59:22 +0300

    v0.1: Initial solution skeleton with Journal, Rules and gitignore

--- AmberNotes/ViewModels/MainViewModel.cs
+++ AmberNotes/ViewModels/MainViewModel.cs
@@ -1,10 +1,17 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace AmberNotes.ViewModels
 {
     public partial class MainViewModel : ViewModelBase
     {
         [ObservableProperty]
-        private string _greeting = "Welcome to Avalonia!";
+        private string _greeting = "Привіт, Amber Notes!";
+
+        [RelayCommand]
+        private void CreateNote()
+        {
+            Greeting = "Бурштинову нотатку створено! ✨";
+        }
     }
 }
